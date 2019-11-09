48f22d4853473812c4f4fd24593eae0ac023b4f9
JJE <elguetaj@ufm.edu>
2019-11-08 23:58:04 -0600

    adds everything

--- public/js/example.js
+++ public/js/example.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.delete').click(function(){
-        let name = $('#target').val()
+        let name = $('#target').data()
         console.log(name)
         $.post('/delete-user', {name:name}, function(data, status) {
             console.log(`${data.message} and status is ${status}`)
